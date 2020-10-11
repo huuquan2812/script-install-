c7be7c97204b775f84abe7eb03ddd4c302a8258b
huuquan2812 <huuquan281297@gmail>
2020-10-11 15:10:25 +0700

    a

--- elgg.sh
+++ elgg.sh
@@ -9,9 +9,5 @@ systemctl status apche2
 #install mysql 
 sudo apt install mysql-server -y
 #install php
-sudo apt install -y  php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
-sudo  apt install -y php libapache2-mod-php
-
-#create database for elgg 
-echo "create database elgg;"| mysql -u root -p"Luonghuuquan1!"| true
+sudo apt-get install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-sqlite3 php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-mysql php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y
 

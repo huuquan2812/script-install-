7c427602c96c963aa27962fce8433cbbd8e6f12b
huuquan2812 <huuquan281297@gmail>
2020-10-11 14:51:50 +0700

    a

--- elgg.sh
+++ elgg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #update&&upgrade system 
-sudo apt-get update -y$$sudo apt-get upgrade -y
+sudo apt-get update -y $$ sudo apt-get upgrade -y
 #install lamp sever 
 #install apache2 
 sudo apt install apache2 
@@ -11,6 +11,6 @@ sudo apt install mysql-server -y
 apt-get install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-sqlite3 php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-mysql php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y 
 sudo systemctl restart apache2
 #create database for elgg 
-echo "create database elgg;"| mysql -u root -p"Luonghuuquan1!"
+echo "create database elgg;"| mysql -u root -p"Luonghuuquan1!"|true
 echo "GRANT ALL PRIVILEGES on elgg.* to 'quan_ellg'@'localhost' identified by 'Luonghuuquan1!';"|mysql -u root -p"Luonghuuquan1!"
 

4fab2429f4a19868b96e7f669031b5d46002a155
huuquan2812 <huuquan281297@gmail>
2020-10-11 14:24:31 +0700

    update script

--- elgg.sh
+++ elgg.sh
@@ -11,15 +11,15 @@ sudo apt install mysql-server -y
 apt-get install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-sqlite3 php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-mysql php7.2-gd php7.2-xml php7.2-cli php7.2-zip -y 
 sudo systemctl restart apache2
 #create database for elgg 
-echo" create database elgg ;"| mysql -u root -p"Luonghuuquan1!"
-echo"GRANT ALL PRIVILEGES on elgg.* to 'quan_ellg'@'localhost' identified by 'Luonghuuquan1!';"|mysql -u root -p"Luonghuuquan1!"
+echo "create database elgg;"| mysql -u root -p"Luonghuuquan1!"
+echo "GRANT ALL PRIVILEGES on elgg.* to 'quan_ellg'@'localhost' identified by 'Luonghuuquan1!';"|mysql -u root -p"Luonghuuquan1!"
 #dowload the lastest version ellg 
 cd /var/www/html 
-rm -r index.html 
-wget https://elgg.org/download/elgg-2.3.7.zip
-apt install unzip
+sudo rm -r index.html 
+sudo wget https://elgg.org/download/elgg-2.3.7.zip
+sudo apt install unzip
 unzip elgg-2.3.7.zip
-mv ./elgg-2.3.7/* . && rm elgg-2.3.7.zip && rm -r elgg-2.3.7
+sudo mv ./elgg-2.3.7/* . && rm elgg-2.3.7.zip && rm -r elgg-2.3.7
 #Create a data directory for Elgg.
 sudo mkdir -p /var/www/html/data
 sudo chown -R www-data:www-data /var/www/html/

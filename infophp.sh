dceb4fd8b7bb8bd15d3ef50974c067774a59b799
huuquan2812 <huuquan281297@gmail>
2020-10-17 18:27:35 +0700

    up

--- infophp.sh
+++ infophp.sh
@@ -2,5 +2,5 @@
 sudo mv ./index.php /var/www/html 
 sudo mv ./oanh.php.conf /etc/apache2/sites-available
 sudo a2ensite oanh.php.conf 
-sudo systemctl restart apache2 
+sudo systemctl reload apache2 
 
